a2bb95f26dce7088e57600173238b682aa937bf1
Juan Pablo Luna <jp_luna@hotmail.com>
2024-02-10 12:29:37 -0300

    Modificación de Templates

--- AppMantenimientoHogar/urls.py
+++ AppMantenimientoHogar/urls.py
@@ -4,7 +4,7 @@ from AppMantenimientoHogar import views
 
 urlpatterns = [
     path('', views.index, name="Index"),
-    path('about/', views.about, name="Views"),
+    path('about/', views.about, name="About"),
     path('contact/', views.contact, name="Contact"),
     path('service/', views.service, name="Service"),      
 ]
