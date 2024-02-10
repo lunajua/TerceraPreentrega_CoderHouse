4921a7eca2b5bd34288073f2372e5f445d2cd921
Juan Pablo Luna <jp_luna@hotmail.com>
2024-02-10 12:40:17 -0300

    modificaciones varias

--- AppMantenimientoHogar/models.py
+++ AppMantenimientoHogar/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=50)
+    celular = models.IntegerField(max_length=50)
+    email = models.EmailField(null=True, blank=True)
+    mensaje = models.TextField() 
+    
+class Persona(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    fecha_nacimiento = models.DateField()  
+    
